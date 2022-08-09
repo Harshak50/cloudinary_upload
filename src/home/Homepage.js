f2b064847c24956b7455961abb5236fd69a9d9f1
Harsha <konndapuramharsha@gmail.com>
2022-08-10 01:01:38 +0530

    alignment issues

--- src/home/Homepage.js
+++ src/home/Homepage.js
@@ -8,122 +8,140 @@ const HomePage = () => {
   return (
     <>
       <Navbar />
-        <Box
-          maxWidth="md"
-          sx={{
-            zIndex: "tooltip",
-            bgcolor: "#F5FAFB",
-            height: 450,
-            margin: "7% auto",
-            borderRadius: "20px",
-          }}
-        >
-          <Stack direction="row">
-            <img
-              alt="img"
-              src={img}
+      <Box
+        maxWidth="md"
+        sx={{
+          "@media (max-width: 800px)": {
+            width:350,
+            height:500
+          },
+          zIndex: "tooltip",
+          bgcolor: "#F5FAFB",
+          height: 450,
+          margin: "7% auto",
+          borderRadius: "20px",
+        }}
+      >
+        <Stack direction="row">
+
+          <img
+            alt="img"
+            src={img}
+            style={{
+              display: { xs: "none", md: "block" },
+              height: "15%",
+              width: "15%",
+              margin: "7% auto",
+              transform: "rotate(10deg)",
+            }}
+          ></img>
+
+          <Box sx={{ margin: "auto", marginLeft: "0%" }}>
+            <Typography
               style={{
-                height: "15%",
-                width: "15%",
-                margin: "7% auto",
-                transform: "rotate(10deg)",
+                fontSize: "35px",
+                "@media (max-width: 500px)": {
+                  fontSize: "15px",
+                },
               }}
-            ></img>
-            <Box sx={{ margin: "auto", marginLeft: "0%" }}>
-              <Typography
-                style={{ fontSize: "35px" }}
-                color="primary"
-                fontFamily="Poppins"
-                className="upload_title"
-              >
-                Secure Cloud Storage &
-              </Typography>
-              <Typography
-                style={{ fontSize: "35px", color: "#1B1A17" }}
-                fontFamily="Poppins"
-                className="upload_title"
-              >
-                Media Platform
-              </Typography>
-              <Typography style={{ color: "#3a36e4" }}>
-                Grab Premium Account today. Check out our awesome deal!
-              </Typography>
-            </Box>
-          </Stack>
-        </Box>
+              color="primary"
+              fontFamily="Poppins"
+              className="upload_title"
+            >
+              Secure Cloud Storage &
+            </Typography>
+            <Typography
+              style={{
+                fontSize: "35px",
+                color: "#1B1A17",
+                "@media (max-width: 800px)": {
+                  fontSize: "15px",
+                },
+              }}
+              fontFamily="Poppins"
+              className="upload_title"
+            >
+              Media Platform
+            </Typography>
+            <Typography style={{ color: "#3a36e4" }}>
+              Grab Premium Account today. Check out our awesome deal!
+            </Typography>
+          </Box>
+        </Stack>
+      </Box>
 
-        <Box maxWidth="xs"
+      <Box
+        maxWidth="xs"
+        sx={{
+          bgcolor: "#fff",
+          height: 300,
+          "@media (min-width: 780px)": {
+            width: 500,
+            transform: "translate(0%, -100%)",
+          },
+          width: 350,
+          justifyContent: "center",
+          border: "1px solid #fff",
+          boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px",
+          borderRadius: 5,
+          fontSize: "0.875rem",
+          transform: "translate(0%, -60%)",
+          fontWeight: "700",
+          margin: "auto",
+          zIndex: "modal",
+        }}
+      >
+        <Box
           sx={{
-            bgcolor: "#fff",
-            height: 300,
-            '@media (min-width: 780px)' : {
-              width: 500,
-              transform: "translate(0%, -100%)",
-            },
-            width: 200,
+            display: "flex",
             justifyContent: "center",
-            border: "1px solid #fff",
-            boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px",
+            alignItems: "center",
             borderRadius: 5,
-            fontSize: "0.875rem",
-            transform: "translate(0%, -60%)",
-            fontWeight: "700",
-            margin: "auto",
-            zIndex: "modal",
+            bgcolor: "#F5FAFB",
+            height: 250,
+            transform: "translate(0%, -3%)",
+            width: "88%%",
+            margin: "6%",
           }}
         >
-
-
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              borderRadius: 5,
-              bgcolor: "#F5FAFB",
-              height: 250,
-              transform: "translate(0%, -3%)",
-              width: "88%%",
-              margin: "6%",
-            }}>
-            <Stack direction="column">
-              <Typography
+          <Stack direction="column">
+            <Typography
               color="primary"
-                fontFamily="Poppins"
-                sx={{ color: "#cccccc", display: "block" }}
-              >
-                Drag & Drop files here to upload
-              </Typography>
-              <Button
-                variant="contained"
-                className="button"
-                fontFamily="Poppins"
-                sx={{
-                  display: "block",
-                  fontWeight: "500",
-                  fontSize: "15px",
-                  textTransform: "none",
+              fontFamily="Poppins"
+              sx={{ color: "#cccccc", display: "block" }}
+            >
+              Drag & Drop files here to upload
+            </Typography>
+            <Button
+              variant="contained"
+              className="button"
+              fontFamily="Poppins"
+              sx={{
+                display: "block",
+                fontWeight: "500",
+                fontSize: "15px",
+                textTransform: "none",
 
-                  margin: "20px",
-                  borderRadius: "5px",
-                  ':hover': {
-                    backgroundColor:'#ff5d3d',
-                    color: '#ffffff',
-                  },
-                }}
+                margin: "20px",
+                borderRadius: "5px",
+                ":hover": {
+                  backgroundColor: "#ff5d3d",
+                  color: "#ffffff",
+                },
+              }}
+            >
+              {" "}
+              <Typography
+                fontFamily="Poppins"
+                sx={{ color: "white", display: "block" }}
               >
                 {" "}
-                <Typography
-                  fontFamily="Poppins"
-                  sx={{ color: "white", display: "block" }}> Upload Image</Typography>
-              </Button>
-            </Stack>
-          </Box>
-
-
-
-
+                Upload Image
+              </Typography>
+            </Button>
+          </Stack>
         </Box>
+      </Box>
     </>
   );
 };
